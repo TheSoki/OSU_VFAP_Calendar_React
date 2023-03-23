51beb5effb2790d249a9e02d6b5b72864ad460a6
Marek Sokol <devmareksokol@gmail.com>
2023-03-23 02:27:07 +0100

    chore: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+    {
+        path: '/',
+        element: <div>Hello world!</div>,
+    },
+])
 
 const App = () => {
-  return <RouterProvider router={router} />;
-};
+    return <RouterProvider router={router} />
+}
 
-export default App;
+export default App
