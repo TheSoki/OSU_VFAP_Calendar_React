aaf5803aaaadeb9277ccd6913ecdb026c755ebcf
Marek Sokol <devmareksokol@gmail.com>
2023-03-31 23:58:15 +0200

    chore: setup tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export const Index = () => {
     }, [])
 
     return (
-        <div>
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <h1>Index</h1>
         </div>
     )
